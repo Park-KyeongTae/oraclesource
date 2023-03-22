1a86c15a4fdb3da74d3b72ad3b5b9b9c8e55ac5b
parkkyeongtae <parkkt96@naver.com>
2023-03-22 18:04:34 +0900

    select 문제

--- hr.sql
+++ hr.sql
@@ -229,9 +229,128 @@ WHERE
     commission_pct IS NOT NULL;
     
     
-    
-    
-    
+-- join 
+-- 자신의 담당 매니저의 고용일 보다 빠른 입사자 찾기(self -join, employees)
+-- hire_date, last_name, manager_id 조회
+
+select e1.hire_date, e1.last_name, e1.manager_id
+from employees e1, employees e2
+where e1.manager_id = e2.employee_id and e1.hire_date<e2.hire_date;
+
+--도시 이름이 t로 시작하는 지역에 사는 사원들의 사번, last_name, 부서번호, 도시 조회
+--employees, departments inner join
+
+SELECT
+    e.employee_id,
+    e.last_name,
+    d.department_id,
+    l.city
+FROM
+    employees   e,
+    departments d,
+    locations   l
+WHERE
+        e.department_id = d.department_id
+    AND d.location_id = l.location_id
+    AND l.city LIKE 'T%';
+--
+--employees, departments inner join
+
+SELECT
+    e.employee_id,
+    e.last_name,
+    d.department_id,
+    e.salary
+FROM
+    employees   e,
+    departments d
+WHERE
+        e.department_id = d.department_id
+    AND d.location_id = 1700;
+
+--부서명, 위치id ,각 부서별 사원 총 수, 각 부서별 평균 연봉 조회
+-- 평균 연봉은 소수점 2자리까지만
+-- empoyees, department inner join
+
+SELECT
+    d.department_name,
+    d.location_id,
+    count(e.employee_id),
+    round(avg(e.salary),2)
+FROM
+    employees   e,
+    departments d
+WHERE
+    e.department_id = d.department_id
+group by
+    d.department_name,
+    d.location_id;
+
+-- Executive 부서에 근무하는 사원들의 부서번호,last_name,job_id 조회
+-- employees, department inner join
+
+SELECT
+    d.department_id,
+    e.last_name,
+    e.job_id
+FROM
+    employees   e,
+    departments d
+WHERE
+    e.department_id = d.department_id and d.department_name = 'Executive';
+
+-- 각 사원별 소속부서에서 자신보다 늦게 고용되었으나 보다 많은 연봉을 받는 사원이 존재하는 모든사원들의
+-- 부서번호, 이름(first_name, last_name 연결하기),salary, hire_date 조회
+-- employees self join
+
+SELECT distinct
+    e1.department_id,
+    e1.first_name
+    || ' '
+    || e1.last_name AS name,
+    e1.salary,
+    e1.hire_date
+FROM
+    employees e1,
+    employees e2
+WHERE
+        e1.department_id = e2.department_id
+    AND e1.hire_date < e2.hire_date
+    AND e1.salary < e2.salary;
+
+-- 서브퀴리 
+-- LAST_NAME에 u가 포함된 사원들과 동일 부서에 근무하는 사원들의 사번, LAST_NAME 조회
+
+select employee_id, last_name
+from employees e, departments d
+where e.department_id = d.department_id and last_name like '%u%';
+
+
+-- JOB_ID 가 SA_MAN 인 사원들의 최대 연봉보다 높게 받는 사원들의 LAST_NAME,JOB_ID,SALARY 조회
+select LAST_NAME,JOB_ID, max(SALARY)
+from employees
+where job_id = 'SA_MAN'; in < (select sal from employees where job_id !='SA_MAN');
+
+
+
+-- 커미션을 버는 사원들의 부서와 연봉이 동일한 사원들의 LAST_NAME,DEPARTMENT_ID,SALARY 조회
+select LAST_NAME,DEPARTMENT_ID,SALARY
+from employees 
+where commission_pct is not null in (select DEPARTMENT_ID,SALARY from employees);
+
+
+
+--회사 전체 평균 연봉보다 더 받는 사원들 중 LAST_NAEM에 u가 있는 사원들이 근무하는 부서에서 
+--근무하는 사원들의 employee_id, last_name, salary 조회
+
+
+
+-- last_name 이 Davies 인 사람보다 나중에 고용된 사원들의 last_name, hire_date 조회
+select last_name, hire_date
+from employees
+where last_name = 'Davies' ;
+
+-- last_name 이 King 인 사원을 매니저로 두고 있는 모든 사원들의 last_name, salary 조회
     
     
     
